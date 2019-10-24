e19076bcc164d8f23fc5a6d21384f71d3b92596e
filinouns <andpuent@ucm.es>
2019-10-24 18:58:28 +0200

    10 y 13

--- Juez/Ejercicio10/Main.cpp
+++ Juez/Ejercicio10/Main.cpp
@@ -5,22 +5,87 @@
 #include <iostream>
 #include <iomanip>
 #include <fstream>
+#include <functional>
+#include <queue>
+
+#include "Grafo.h"
 
 using namespace std;
 
-// función que resuelve el problema
-bool resolver() {
-	return true;
-}
+class bst {
+public:
+
+	bst(const Grafo &g) : marked_(g.V(), false), dist_(g.V()) {
+		bip = true;
+
+		//Recorremos los m elementos del grafo
+		for (int i = 0; i < g.V(); i++) {
+			//Si no esta marcado llamamos a resolverlo, de esta manera nos aseguramos que recorremos los elementos sueltos
+			if (!marked_[i]) {
+				resolve(g, i);
+			}
+		}
+	}
+
+	void solucion() {
+		if (bip) cout << "SI\n";
+		else cout << "NO\n";
+	}
+
+
+private:
+	vector <bool> marked_;
+	vector <int> dist_;
+
+	bool bip;
+
+	void resolve(const Grafo &g, int e) {
+		// Creamos una cola de prioridad con el elemento (e)
+		priority_queue<int> q;
+		dist_[e] = 0; //La distancia a e se toma como 0
+		marked_[e] = true;
+		q.push(e);
+
+		//Mientras que sigamos escuchando el latido (bip) y haya elementos en la cola, comprobamos:
+		while (!q.empty() && bip) {
+			int v = q.top(); q.pop(); //Cogemos el elem
+			for (int z : g.ady(v)) { //Recorremos los hijos del elem
+				if (!marked_[z]) { // Si no estan los hijos marcados
+					dist_[z] = dist_[v] + 1; //Le damos 1 mas de distancia del elemento (v)
+					marked_[z] = true; //Marcamos
+					q.push(z); //Lo añadimos a la cola para que se recorran sus hijos
+				}
+				else { // Si el hijo esta marcado se comprobara que la distancia entre ellos sea siempre diferente
+					if (dist_[z] % 2 == 0 && dist_[v] % 2 == 0) bip = false;
+					else if (dist_[z] % 2 != 0 && dist_[v] % 2 != 0) bip = false;
+				}
+			}
+		}
+	}
+};
 
 // Resuelve un caso de prueba, leyendo de la entrada la
 // configuración, y escribiendo la respuesta
 bool resuelveCaso() {
 	// leer los datos de la entrada
 
-	if (!cin)
-		return false;
+	long V, E;
 
+	cin >> V;
+	cin >> E;
+
+	if (!cin) return false;
+
+	Grafo g(V);
+	for (int i = 0; i < E; i++) {
+		long v, w;
+		cin >> v >> w;
+
+		g.ponArista(v, w);
+	}
+
+	bst sol(g);
+	sol.solucion();
 
 	return true;
 }
--- Juez/Ejercicio13.cpp
+++ Juez/Ejercicio13.cpp
@@ -2,16 +2,76 @@
 // MARP34
 // Problema13 -> La ronda de la noche
 
-// Usar grafos para representar el mapa, a cada vertice se le deben añadir como aristas los 4, o menos, vertices laterales
-
 #include <iostream>
 #include <iomanip>
 #include <fstream>
 #include <functional>
 #include <queue>
+#include <vector>
+
+#include "Grafo.h"
 
 using namespace std;
 
+class bst {
+public:
+	bst(const Grafo &g, vector<char> &d, int ini) : marked_(g.V(), false), dist_(g.V()), bip(false) {
+		resolve(g, d, ini);
+	}
+
+	//void searchCamino(int ini)
+
+	void solucion() {
+		if (bip) cout << dist_[exit] << "\n";
+		else cout << "NO\n";
+	}
+
+
+private:
+	vector<bool> marked_;
+	vector<int> dist_;
+
+	int exit;
+	bool bip;
+
+	void resolve(const Grafo &g, vector<char> &d, int e) {
+		priority_queue<int> q;
+		marked_[e] = true;
+		dist_[e] = 0;
+		q.push(e);
+
+		while (!q.empty() && !bip) {
+			int v = q.top(); q.pop();
+			for (int z : g.ady(v)) {
+				switch (d[z]) {
+				case '.':	// En caso de poder seguir el camino
+					if (!marked_[z]) {
+						marked_[z] = true;
+						dist_[z] = dist_[v] + 1;
+						q.push(z);
+					}
+					break;
+				case '#':	// En caso de encontrar una pared
+					if (!marked_[z]) marked_[z] = true;
+					break;
+				case 'P':	// En caso de encontrar la salida
+					if (!marked_[z]) {
+						marked_[z] = true;
+						dist_[z] = dist_[v] + 1;
+						exit = z;
+						bip = true;
+					}
+					break;
+				case 'E':
+					break;
+				default: //Usarlo aqui para las camaras, ya que no hay casos que las incluyan
+					if (!marked_[z]) marked_[z] = true;
+					break;
+				}
+			}
+		}
+	}
+};
 
 // Resuelve un caso de prueba, leyendo de la entrada la
 // configuración, y escribiendo la respuesta
@@ -26,8 +86,88 @@ bool resuelveCaso() {
 	for (int i = 0; i < n_Gardens; i++) {
 		cin >> ancho;
 		cin >> alto;
-	}
 
+		int v = ancho * alto;
+
+		priority_queue<int> cameras;
+		vector<char> data;
+		Grafo g(v);
+
+		int init;
+
+		for (int i = 0; i < alto; i++) {
+			for (int j = 0; j < ancho; j++) {
+				int c1 = j + i * ancho;			//Arista inicial que debe unirse a las dos inferiores
+				int c2 = c1 + 1;				//Arista a la der de la inicial
+				int c3 = j + (i + 1) * ancho;	//Arista bajo la inicial
+				
+				//Comprobaciones para no pasarnos de ancho/largo del tablero
+				if(j < ancho - 1) g.ponArista(c1, c2);
+				if(i < alto - 1) g.ponArista(c1, c3);
+
+				// Info de la casilla actual
+				char info;
+				cin >> info;
+				data.push_back(info);
+
+				if (info == 'E') init = data.size() - 1;	// Guardamos la posicion de la entrada para comenzar a buscar desde ahi
+				else if (info - '0' >= 0 && info - '9' <= 0) cameras.push(data.size() - 1);
+			}
+		}
+
+		// Actualizacion del tablero con las distancias de las camaras
+		while(!cameras.empty()) {
+			int cam = cameras.top(); cameras.pop();
+			char vision = data[cam];
+
+			if (vision != '0') {
+				if (vision == '1') {
+					for (int z : g.ady(cam)) {
+						data[z] = vision;
+					}
+				}
+				else {
+					int size = vision - '0';
+					int camV = 0, limIzq = 0, limDer = 0;
+					int auxH = ancho;
+					bool bIzq = true, bDer = true, bUp = true, bDown = true;
+					for (int i = 0; i < size; i++) {
+						// Hacia abajo
+						camV = cam - auxH;
+						if (camV >= 0 && bDown) {
+							if(data[camV] == '.') data[camV] = vision;
+							else bDown = false;
+						}
+						//Hacia arriba
+						camV = cam + auxH;
+						if (camV <= alto * ancho && bUp) {
+							if (data[camV] == '.') data[camV] = vision;
+							else bUp = false;
+						}
+						//Hacia la izq
+						camV = cam - (i + 1);
+						limIzq = (camV / ancho) * ancho;
+						if(camV >= limIzq && bIzq){
+							if (data[camV] == '.') data[camV] = vision;
+							else bIzq = false;
+						}
+						//Hacia la der
+						camV = cam + (i + 1);
+						limDer = ((camV / ancho) + 1) * ancho;
+						if (camV <= limDer && bDer) {
+							if (data[camV] == '.') data[camV] = vision;
+							else bDer = false;
+						}
+
+						auxH += ancho;
+					}
+				}
+			}
+		}
+
+		bst b(g, data, init);
+		b.solucion();
+	}
 
 	return true;
 }
@@ -40,7 +180,7 @@ int main() {
 	auto cinbuf = std::cin.rdbuf(in.rdbuf()); //save old buf and redirect std::cin to casos.txt
 #endif 
 
-	while (resuelveCaso());
+	resuelveCaso();
 
 	// Para restablecer entrada. Comentar para acepta el reto
 #ifndef DOMJUDGE // para dejar todo como estaba al principio
